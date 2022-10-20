05e26b921ab80d5acf03fdff1f1743f2031d798d
JackSparrow456 <sherlockholmes001.in@gmail.com>
2022-10-20 12:14:26 +0530

    my task

---
+++ src/org/sel/Test.java
@@ -0,0 +1,22 @@
+package org.sel;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Test {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\loges\\eclipse-workspace\\Seleniumn\\driv\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.green-tech.co.in/");	
+		Thread.sleep(3000);
+		WebElement drpProduct = driver.findElement(By.xpath("//li[@class='drop']"));
+		Actions actions = new Actions(driver);
+
+		actions.moveToElement(drpProduct).perform();
+WebElement awstraining = driver.findElement(By.xpath("//a[@style='padding-top: 8px; padding-bottom: 8px;']//ancestor::li//a[@href='paperbagmachine.html']"));
+awstraining.click();
+	}
+}
